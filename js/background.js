fb173e26ebe794406516689280b19cfb8725a7bf
Grobato <yushurpg@hotmail.com>
2017-09-08 23:34:06 -0400

    updated

--- js/background.js
+++ js/background.js
@@ -5,17 +5,17 @@ var select = document.getElementById("background");
 select.addEventListener("change", function(event) {
 
   if (event.target.value === "a") {
-    document.body.style.backgroundImage = 'url("http://zupimages.net/up/17/36/8onf.jpg")';
-    document.body.style.backgroundPosition = "bottom";
+    document.body.style.backgroundImage = 'url("https://s2.qwant.com/thumbr/0x0/e/3/b4ebad6423120ad73eddb9ba8398f8/b_1_q_0_p_0.jpg?u=https%3A%2F%2Fwww.taberhols.co.uk%2Ffiles%2FIceland%2Fvik-iceland-holiday.jpg&q=0&b=1&p=0&a=1")';
+    document.body.style.backgroundPosition = "top";
   } else if (event.target.value === "d") {
-    document.body.style.backgroundImage = 'url("https://drive.google.com/open?id=0Bw__udsOLruRR1pFM3VJZmRLZ2M")';
+    document.body.style.backgroundImage = 'url("https://s1.qwant.com/thumbr/0x0/1/3/9a652e99775e13559b7b6539f36e7c/b_1_q_0_p_0.jpg?u=http%3A%2F%2Fhdwallpapers.move.pk%2Fwp-content%2Fuploads%2F2015%2F03%2Fmountain-snow.jpg&q=0&b=1&p=0&a=1")';
     document.body.style.backgroundPosition = "top";
   } else if (event.target.value === "b") {
-    document.body.style.backgroundImage = 'url("https://drive.google.com/open?id=0Bw__udsOLruRZnJuUFRseFo0cGM")';
-    document.body.style.backgroundPosition = "top";
+    document.body.style.backgroundImage = 'url("https://www.consciouslifestylemag.com/wp-content/uploads/2015/05/Iceland-travel-guide-foggy-road-mystical.jpg")';
+    document.body.style.backgroundPosition = "bottom";
   } else if (event.target.value === "c") {
-    document.body.style.backgroundImage = 'url("https://drive.google.com/file/d/0Bw__udsOLruRZHFlLWFXdjlVeW8")';
-    document.body.style.backgroundPosition = "center";
+    document.body.style.backgroundImage = 'url("https://s2.qwant.com/thumbr/0x0/7/5/b52dc2b63b576ce330352a191f87a5/b_1_q_0_p_0.jpg?u=http%3A%2F%2Fdata.1freewallpapers.com%2Fdownload%2Fautumn-trees-nature-landscape-leaf-leaves-desktop-background-images.jpg&q=0&b=1&p=0&a=1")';
+    document.body.style.backgroundPosition = "top";
   }
 
   document.body.style.backgroundSize = "cover";
