29226b574b3f2a52d80844ff1e0d4d12aacbdb98
Grobato <yushurpg@hotmail.com>
2017-09-08 23:06:55 -0400

    updated

--- js/background.js
+++ js/background.js
@@ -5,17 +5,22 @@ var select = document.getElementById("background");
 select.addEventListener("change", function(event) {
 
   if (event.target.value === "a") {
-    document.body.style.backgroundImage = 'url("https://s-media-cache-ak0.pinimg.com/originals/bc/96/13/bc96139f55637a2b5020d0b57b346e33.jpg")';
+    document.body.style.backgroundImage = 'url("https://drive.google.com/open?id=0Bw__udsOLruRSDY2X09sWVJvMmc")';
+    document.body.style.backgroundPosition = "bottom";
   } else if (event.target.value === "d") {
-    document.body.style.backgroundImage = 'url("http://jonvilma.com/images/mountain-6.jpg")';
+    document.body.style.backgroundImage = 'url("https://drive.google.com/open?id=0Bw__udsOLruRR1pFM3VJZmRLZ2M")';
+    document.body.style.backgroundPosition = "top";
   } else if (event.target.value === "b") {
-    document.body.style.backgroundImage = 'url("https://www.walldevil.com/wallpapers/h14/forest-wallpaper-foggy-nature.jpg")';
+    document.body.style.backgroundImage = 'url("https://drive.google.com/open?id=0Bw__udsOLruRZnJuUFRseFo0cGM")';
+    document.body.style.backgroundPosition = "top";
   } else if (event.target.value === "c") {
-    document.body.style.backgroundImage = 'url("http://www.yumitolesson.com/wp-content/uploads/2015/09/Fall-1.jpg")';
+    document.body.style.backgroundImage = 'url("https://drive.google.com/file/d/0Bw__udsOLruRZHFlLWFXdjlVeW8")';
+    document.body.style.backgroundPosition = "center";
   }
 
-  document.body.style.backgroundSize = 'cover';
-  document.body.style.backgroundRepeat = 'no-repeat';
+  document.body.style.backgroundSize = "cover";
+  document.body.style.backgroundRepeat = "no-repeat";
+  document.body.style.backgroundAttachment = "fixed";
 
 });
 
