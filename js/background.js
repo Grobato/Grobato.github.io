4c929fca760848fc2673cd1457ccd7c643028351
Grobato <yushurpg@hotmail.com>
2017-09-08 22:31:31 -0400

    backgrounds changed

--- js/background.js
+++ js/background.js
@@ -5,13 +5,13 @@ var select = document.getElementById("background");
 select.addEventListener("change", function(event) {
 
   if (event.target.value === "a") {
-    document.body.style.backgroundImage = 'url("https://cdn.pixabay.com/photo/2017/08/18/19/48/iceland-2656257_960_720.jpg")';
+    document.body.style.backgroundImage = 'url("https://s-media-cache-ak0.pinimg.com/originals/bc/96/13/bc96139f55637a2b5020d0b57b346e33.jpg")';
   } else if (event.target.value === "d") {
-    document.body.style.backgroundImage = 'url("https://cdn.pixabay.com/photo/2017/04/14/18/32/mountain-2230986_960_720.jpg")';
+    document.body.style.backgroundImage = 'url("http://jonvilma.com/images/mountain-6.jpg")';
   } else if (event.target.value === "b") {
-    document.body.style.backgroundImage = 'url("https://cdn.pixabay.com/photo/2016/02/24/18/37/fog-1220491_960_720.jpg")';
+    document.body.style.backgroundImage = 'url("https://www.walldevil.com/wallpapers/h14/forest-wallpaper-foggy-nature.jpg")';
   } else if (event.target.value === "c") {
-    document.body.style.backgroundImage = 'url("https://cdn.pixabay.com/photo/2015/12/01/20/28/fall-1072821_960_720.jpg")';
+    document.body.style.backgroundImage = 'url("http://www.yumitolesson.com/wp-content/uploads/2015/09/Fall-1.jpg")';
   }
 
   document.body.style.backgroundSize = 'cover';
