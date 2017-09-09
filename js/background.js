b653b93db3d21226af882a781ad9abbcf8dfd3b4
Grobato <yushurpg@hotmail.com>
2017-09-09 00:23:14 -0400

    updated

--- js/background.js
+++ js/background.js
@@ -8,7 +8,7 @@ select.addEventListener("change", function(event) {
     document.body.style.backgroundImage = 'url("https://s2.qwant.com/thumbr/0x0/e/3/b4ebad6423120ad73eddb9ba8398f8/b_1_q_0_p_0.jpg?u=https%3A%2F%2Fwww.taberhols.co.uk%2Ffiles%2FIceland%2Fvik-iceland-holiday.jpg&q=0&b=1&p=0&a=1")';
     document.body.style.backgroundPosition = "top";
   } else if (event.target.value === "d") {
-    document.body.style.backgroundImage = 'url("https://s1.qwant.com/thumbr/0x0/1/3/9a652e99775e13559b7b6539f36e7c/b_1_q_0_p_0.jpg?u=http%3A%2F%2Fhdwallpapers.move.pk%2Fwp-content%2Fuploads%2F2015%2F03%2Fmountain-snow.jpg&q=0&b=1&p=0&a=1")';
+    document.body.style.backgroundImage = 'url("https://s2.qwant.com/thumbr/0x0/f/6/7f5368732ced90aa84d8f75bdc985c/b_1_q_0_p_0.jpg?u=http%3A%2F%2Fblog.teamalpin.com%2Fwp-content%2Fuploads%2F2012%2F09%2FMount_Fuji_2.jpg&q=0&b=1&p=0&a=1")';
     document.body.style.backgroundPosition = "top";
   } else if (event.target.value === "b") {
     document.body.style.backgroundImage = 'url("https://www.consciouslifestylemag.com/wp-content/uploads/2015/05/Iceland-travel-guide-foggy-road-mystical.jpg")';
