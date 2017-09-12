3fccb0751279d212bc13bf2082eba94642d08e22
Grobato <yushurpg@hotmail.com>
2017-09-12 14:06:03 -0400

    updated, not working atm

---
+++ js/chat.js
@@ -0,0 +1,50 @@
+// creating table chat
+var tableMessages = [{
+  name: "",
+  message: ""
+}];
+
+// variables
+var chatName = document.getElementById("chatname");
+var chatMessage = document.getElementById("chatmessage");
+var chatSubmit = document.getElementById("chatsubmit");
+
+
+
+tableMessages.forEach(function(msg) {
+
+  var displayChat = document.getElementById("displaychat");
+
+  var divElt = document.createElement("div");
+  divElt.setAttribute("class", "displaymessage");
+
+  var spanElt = document.createElement("span");
+  spanElt.setAttribute("class", "boldname");
+  spanElt.fontWeight = "bold";
+  spanElt.textContent = msg.name + ": ";
+
+  var messageElt = document.createTextNode(msg.message);
+
+
+  displayChat.appendChild(divElt);
+  divElt.appendChild(spanElt);
+  divElt.appendChild(messageElt);
+
+
+});
+
+
+// Event click on submit button
+chatsubmit.addEventListener("submit", function(e) {
+
+  if (chatName !== null && chatMessage !== null) {
+    var newMessage = {
+      name: chatName.value,
+      message: chatMessage.value
+    };
+    tableMessages.push(newMessage);
+  } else {
+    document.getElementById("launchchat").textContent = "Name and Message are empty";
+  }
+
+});
